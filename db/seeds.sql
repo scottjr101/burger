8c8411c90fd90caf7dc7d37d52dd83415d3abb6e
Scott Ladd <scottjr101@gmail.com>
2019-11-24 23:43:44 -0500

    added database

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Classic Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cowboy Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Black Bean Burger", false);
